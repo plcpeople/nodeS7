1e344aba30fe9c550a00a734949cb89cb5f9c931
Guilherme Francescon <guilherme.francescon@netsmarttech.com>
2019-08-22 17:42:22 -0300

    Implement legacy interface

---
+++ legacy.js
@@ -0,0 +1,87 @@
+//@ts-check
+
+const S7Endpoint = require('./src/s7endpoint');
+const S7ItemGroup = require('./src/s7itemGroup');
+
+class NodeS7 {
+
+    constructor(opts) {
+        this._ep = null;
+        this._group = null;
+        this._translationCb = null;
+    }
+
+    initiateConnection(param, cb) {
+        this._ep = new S7Endpoint(param);
+        this._group = new S7ItemGroup(this._ep);
+
+        if(this._translationCb){
+            this._group.setTranslationCB(this._translationCb);
+        }
+
+        let cbDone = false;
+        function doCb(err) {
+            if (cbDone) return;
+            cbDone = true;
+            cb(err);
+        }
+        this._ep.once('connect', doCb);
+        this._ep.once('error', doCb);
+    }
+
+    dropConnection(cb) {
+        if(!this._ep) {
+            process.nextTick(() => cb());
+            return;
+        }
+
+        this._ep.disconnect().then(cb).catch(cb);
+        this._ep = null;
+        this._group = null;
+    }
+
+    setTranslationCB(cb) {
+        this._translationCb = cb;
+        if (this._group) {
+            this._group.setTranslationCB(cb);
+        }
+    }
+
+    addItems(item) {
+        this._group.addItems(item);
+    }
+
+    removeItems(item) {
+        this._group.removeItems(item);
+    }
+    
+    readAllItems(cb) {
+        if (!this._group) {
+            process.nextTick(() => cb(true, {}));
+            return;
+        }
+
+        this._group.readAllItems().then(data => {
+            cb(false, data);
+        }).catch(e => {
+            cb(true, {});
+        });
+    }
+
+    writeItems(item, value, cb) {
+        if (!this._group) {
+            process.nextTick(() => cb(true, {}));
+            return;
+        }
+
+        this._group.writeItems(item, value).then(data => {
+            cb(false, data);
+        }).catch(e => {
+            cb(true, {});
+        });
+
+    }
+
+}
+
+module.exports = NodeS7;
